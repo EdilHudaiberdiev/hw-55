e11e1789f2a8610b19ef4c0a66d0b2036cb6ae73
Edil Hudaiberdiev <hudaiberdiev.ed@gmail.com>
2023-11-02 21:29:47 +0600

    added event click and delete ingred

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,66 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import cheeseImg from './assets/cheese.png'
 import meatImg from './assets/meat.png'
 import pattyImg from './assets/patty.png'
 import saladImg from './assets/salad.png'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+interface ingredientsInterface {
+  name: string;
+  count: number;
+  img: string;
+  id: number;
+  price: number;
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [ingredients, setIngredients] = useState<ingredientsInterface[]>([
+    {img: cheeseImg,  name: "Cheese", count: 0, id: 1, price: 10},
+    {img: meatImg,  name: "Bacon", count: 0, id: 2, price: 10},
+    {img: pattyImg,  name: "Meat", count: 0, id: 3,  price: 10},
+    {img: saladImg,  name: "Salad", count: 0, id: 4,  price: 10},
+  ]);
+
+  const countClick = (id: number) => {
+    console.log(id);
+    setIngredients(ingredients.map(item => {
+      if (item.id === id ) {
+        return {
+          ...item,
+          count: item.count + 1
+        }
+      }
+            //синтаксис map
+      return  item;
+    }));
+  };
+
+  const deleteIngred = (id: number) => {
+    setIngredients(ingredients.map(item => {
+      if (item.id === id && item.count > 0) {
+        return {
+          ...item,
+          count: item.count - 1
+        }
+      }
+      return  item
+    }))
+
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-          <img src={cheeseImg} className="logo" alt="Vite logo" />
-          <img src={meatImg} className="logo" alt="Vite logo" />
-          <img src={pattyImg} className="logo" alt="Vite logo" />
-          <img src={saladImg} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="ingredientsList">
+        {ingredients.map(item => (
+          <div className="ingredientsElement"  key={item.id}>
+            <p> {item.name} </p>
+            <img  onClick={()=> countClick(item.id)} src={item.img} className="ingredientsImg" alt={item.name}/>
+            <p>Count {item.count}</p>
+            <button onClick={()=> deleteIngred(item.id)} >x</button>
+           </div>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
